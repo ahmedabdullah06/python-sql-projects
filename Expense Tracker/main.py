297e1b6170615469afdaf9b102aa312c2ab85f90
mranonymous1i <abd.ahd2006@gmail.com>
2026-04-25 21:12:08 -0400

    Added entry removal functionality and updated user input accordingly

--- Expense Tracker/main.py
+++ Expense Tracker/main.py
@@ -11,17 +11,64 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE expenditures (id INT AUTO_INCREMENT PRIMARY KEY, date_purchased DATE, item_name VARCHAR(255), description VARCHAR(255), cost FLOAT)")
+mycursor.execute("CREATE TABLE IF NOT EXISTS expenditures (id INT AUTO_INCREMENT PRIMARY KEY, date_purchased DATE, item_name VARCHAR(255), description VARCHAR(255), cost DECIMAL);")
 
 
 #Functions
 def add_expense(date, name, desc, amount):
-  sql = "INSERT INTO expenditures (date_purchased, item_name, description, cost) VALUES (%s, %s, %s, %s)"
+  sql = "INSERT INTO expenditures (date_purchased, item_name, description, cost) VALUES (%s, %s, %s, %s);"
   mycursor.execute(sql, (date, name, desc, amount))
   mydb.commit()
 
-def total_expense():
-  mycursor.execute("SELECT SUM(cost) FROM expenditures")
+def total_expenses():
+  mycursor.execute("SELECT SUM(cost) FROM expenditures;")
+  result = mycursor.fetchone()
+  print("Total:", result[0])
+
+def remove_expense_id(id):
+  check_sql = "SELECT * FROM expenditures WHERE id=%s;"
+  mycursor.execute(check_sql, (id,))
+  result = mycursor.fetchone()
+
+  if result is None:
+      print("Item with id %s not found.", id)
+      return
+
+  delete_sql = "DELETE FROM expenditures WHERE id = %s;"
+  mycursor.execute(delete_sql, (id,))
+  mydb.commit()
+
+  print("Expense deleted.")
+
+def remove_expense_name(item_name):
+  check_sql = "SELECT * FROM expenditures WHERE item_name = %s;"
+  mycursor.execute(check_sql, (item_name,))
+  result = mycursor.fetchone()
+
+  if result is None:
+      print("Item with item name \"%s\" not found.", item_name)
+      return
+
+  delete_sql = "DELETE FROM expenditures WHERE item_name = %s;"
+  mycursor.execute(delete_sql, (item_name,))
+  mydb.commit()
+
+  print("Expense deleted.")
+
+def expenses_month():
+  sql = """
+  SELECT
+  YEAR(date_purchased),
+  MONTH(date_purchased),
+  SUM(cost)
+  FROM expenditures
+  GROUP BY YEAR(date_purchased), MONTH(date_purchased)
+  ORDER BY YEAR(date_purchased) DESC, MONTH(date_purchased) DESC;
+  """
+  mycursor.execute(sql)
+  result = mycursor.fetchall()
+  for i in result:
+    print(i)
 
 
 while True:
@@ -31,7 +78,7 @@ while True:
   print("1. Add expense")
   print("2. Remove expense")
   print("3. View total expenses")
-  print("4. View expenses by year")
+  print("4. View monthly expenses")
   print("5. Add/replace description")
   print("6. Print table")
 
@@ -45,10 +92,30 @@ while True:
       choice2 = input("Would you like to add an item description? y/n: ")
       description = "None"
 
-      if(choice2.capitalize() == "Y" or choice2.capitalize() == "YES"):
+      if choice2.lower() in ('y', 'yes'):
         description = input("Enter a short description: ")
 
       add_expense(date, item, description, cost)
+
+    case "2":
+      print("1. Remove by ID")
+      print("2. Remove by item name")
+
+      choice2 = input("Choose your method of deletion: ")
+      
+      if(choice2 == "1"):
+        id = input("Enter the ID for the item you would like to delete: ")
+        remove_expense_id(id)
+
+      if(choice2 == "2"):
+        item_name = input("Enter the name for the item you would like to delete: ")
+        remove_expense_name(item_name)
+
+    case "3":
+      total_expenses()
+    
+    case "4":
+
 
 
 
